46d1a9184afdfd8141128aac6ad263795859eaca
Paul Standley <paulstandley1972@gmail.com>
2020-11-14 20:41:25 +0000

    enum classes and game consts

--- BlackjackClass.cpp
+++ BlackjackClass.cpp
@@ -4,6 +4,46 @@
 #include "pch.h"
 #include "BlackjackHeader.h"
 #include <iostream>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <ctime>
+#include <random>
+
+// Maximum score before lossing
+constexpr int maximumScore{ 21 };
+
+// Minimum score that the dealer has to have
+constexpr int mmimimumDealerScore{ 17 };
+
+enum class CardSuit
+{
+    SUIT_CLUB,
+    SUIT_DIAMOND,
+    SUIT_HEART,
+    SUIT_SPADE,
+
+    MAX_SUITS
+};
+
+enum class CardRank
+{
+    RANK_2,
+    RANK_3,
+    RANK_4,
+    RANK_5,
+    RANK_6,
+    RANK_7,
+    RANK_8,
+    RANK_9,
+    RANK_10,
+    RANK_JACK,
+    RANK_QUEEN,
+    RANK_KING,
+    RANK_ACE,
+
+    MAX_RANKS
+};
 
 
 int main()
